e4d874b57423401c3ae3e475df24727b2d75a4a6
Hernan <hacc.code@gmail.com>
2023-10-04 08:18:46 -0300

    JSON Carrito y Renderizado de img

--- js/script.js
+++ js/script.js
@@ -1,20 +1,5 @@
-/* let nombre;
-do {
-  nombre = prompt("Ingrese su Nombre");
-  if (!isNaN(nombre) || nombre.trim() === "") {
-    alert("Por favor, ingrese un nombre para continuar");
-  }
-} while (!isNaN(nombre) || nombre.trim() === "");
+/* Variables */ //////////////////////////
 
-let dni;
-do {
-  dni = Number(prompt("Ingrese su DNI"));
-  if (isNaN(dni) || dni <= 0) {
-    alert("Debe ser un número mayor que 0.");
-  }
-} while (isNaN(dni) || dni <= 0); */
-
-//Variables
 let productos = [
   {
     id: "1001",
@@ -467,248 +452,142 @@ let productos = [
     rutaImagen: "dmt.png",
   },
 ];
-let carrito = [];
 let productoBuscado;
+let carrito = [];
+let carritoStandBy = localStorage.getItem("carrito")
+if (carritoStandBy) {
+  carrito = JSON.parse(carritoStandBy);
+}
 
-//Menu Principal
-let opcion;
+renderizarCarrito(carrito)
+
+renderizarProductos(productos, carrito);
+
+///////////////////////////
+/* let nombre;
 do {
-  opcion = parseInt(
-    prompt(
-      "Selecciona una opción:\n1. Reservar entradas para el Parque\n2. Tienda\n3. Festeja tu Cumple \n4. Retira tu Foto \n0. Salir"
-    )
+  nombre = prompt("Ingrese su Nombre");
+  if (!isNaN(nombre) || nombre.trim() === "") {
+    mensajeError()
+  }
+} while (!isNaN(nombre) || nombre.trim() === "");
+
+let dni;
+do {
+  dni = Number(prompt("Ingrese su DNI"));
+  if (isNaN(dni) || dni <= 0) {
+    alert("Debe ser un número mayor que 0.");
+  }
+} while (isNaN(dni) || dni <= 0); */
+
+////////////////////////////////
+/* Reserva de Entradas */ ////////////////////////////
+
+//Precios Entradas segun edad
+let entradaGral = 2200;
+let descMayores = 0.75; //-75% de descuento
+let descMenores = 0.5; //-50% de descuento
+let descInfantes = "Entrada Gratuita";
+/* alert(
+  "Valor entrada: $" +
+    entradaGral +
+    "\nDescuento Mayores de 65 años: -" +
+    descMayores * 100 +
+    "%" +
+    "\nDescuento Menores de 13 años: -" +
+    descMenores * 100 +
+    "%" +
+    "\nDescuento Menores de 6 años: " +
+    descInfantes
+); */
+
+//Bucle Cantidad de entradas y Datos por entrada
+let entradas, dniEntrada, nombreEntrada, apellidoEntrada, edadEntrada;
+
+/* do {
+  entradas = Number(prompt("Cuantas entradas desea reservar?"));
+  if (isNaN(entradas) || entradas <= 0) {
+    alert("Debe ser un número mayor que 0.");
+  } else if (entradas >= 10) {
+    alert("Máximo Reserva: 10 entradas");
+  }
+} while (isNaN(entradas) || entradas <= 0 || entradas >= 10);
+
+for (let i = 1; i <= entradas; i++) {
+  do {
+    dniEntrada = Number(prompt("Ingrese el DNI para visitante Nº " + i));
+    validarDatoNum(dniEntrada);
+  } while (isNaN(dniEntrada) || dniEntrada <= 0);
+
+  do {
+    nombreEntrada = prompt("Ingrese Nombre");
+    validarDatoStr(nombreEntrada);
+  } while (!/^[a-zA-Z]+$/.test(nombreEntrada) || nombreEntrada.trim() === "");
+
+  do {
+    apellidoEntrada = prompt("Ingrese Apellido");
+    validarDatoStr(apellidoEntrada);
+  } while (
+    !/^[a-zA-Z]+$/.test(apellidoEntrada) ||
+    apellidoEntrada.trim() === ""
   );
 
-  switch (
-    opcion //Switch Menu Principal
-  ) {
-    case 1: //Reserva de Entradas
-      //Precios Entradas segun edad
-      let entradaGral = 2200;
-      let descMayores = 0.75; //-75% de descuento
-      let descMenores = 0.5; //-50% de descuento
-      let descInfantes = "Entrada Gratuita";
-      alert(
-        "Valor entrada: $" +
-          entradaGral +
-          "\nDescuento Mayores de 65 años: -" +
-          descMayores * 100 +
-          "%" +
-          "\nDescuento Menores de 13 años: -" +
-          descMenores * 100 +
-          "%" +
-          "\nDescuento Menores de 6 años: " +
-          descInfantes
-      );
+  do {
+    edadEntrada = Number(prompt("Ingrese edad actual del visitante"));
+    validarDatoNum(edadEntrada);
+  } while (isNaN(edadEntrada) || edadEntrada <= 0); */
 
-      //Bucle Cantidad de entradas y Datos por entrada
-      let entradas, dniEntrada, nombreEntrada, apellidoEntrada, edadEntrada;
+let valorEntrada = entradaGral;
 
-      do {
-        entradas = Number(prompt("Cuantas entradas desea reservar?"));
-        if (isNaN(entradas) || entradas <= 0) {
-          alert("Debe ser un número mayor que 0.");
-        } else if (entradas >= 10) {
-          alert("Máximo Reserva: 10 entradas");
-        }
-      } while (isNaN(entradas) || entradas <= 0 || entradas >= 10);
+//condicional valor entradas segun edad
+if (edadEntrada < 6) {
+  valorEntrada = descInfantes;
+} else if (edadEntrada < 13) {
+  valorEntrada = entradaGral - entradaGral * descMenores;
+} else if (edadEntrada >= 65) {
+  valorEntrada = entradaGral - entradaGral * descMayores;
+}
 
-      for (let i = 1; i <= entradas; i++) {
-        do {
-          dniEntrada = Number(prompt("Ingrese el DNI para visitante Nº " + i));
-          validarDatoNum(dniEntrada);
-        } while (isNaN(dniEntrada) || dniEntrada <= 0);
+//Impresion por pantalla de Datos ingresados
+/* alert(
+    "Entrada " +
+      i +
+      ":\nDNI: " +
+      dniEntrada +
+      "\nNombre Visitante: " +
+      nombreEntrada +
+      " " +
+      apellidoEntrada +
+      "\nEdad del Visitante: " +
+      edadEntrada +
+      "\nValor Entrada: $" +
+      valorEntrada
+  ); */
 
-        do {
-          nombreEntrada = prompt("Ingrese Nombre");
-          validarDatoStr(nombreEntrada);
-        } while (
-          !/^[a-zA-Z]+$/.test(nombreEntrada) ||
-          nombreEntrada.trim() === ""
-        );
+//mensaje reserva exitosa
+/* alert(
+  "Reserva Exitosa.!" +
+    nombre +
+    "!\nRecuerde asistir dentro de los 10 días posteriores de la reserva, con DNI de los visitantes sin excepción\nTu Número de reserva es " +
+    dni +
+    " \n¡Muchas gracias! Te esperamos! Parque Tecno ☼"
+); */
 
-        do {
-          apellidoEntrada = prompt("Ingrese Apellido");
-          validarDatoStr(apellidoEntrada);
-        } while (
-          !/^[a-zA-Z]+$/.test(apellidoEntrada) ||
-          apellidoEntrada.trim() === ""
-        );
 
-        do {
-          edadEntrada = Number(prompt("Ingrese edad actual del visitante"));
-          validarDatoNum(edadEntrada);
-        } while (isNaN(edadEntrada) || edadEntrada <= 0);
+///////////////////////////
+/* Funciones */ //////////////////////////////////
 
-        let valorEntrada = entradaGral;
+/* //Funciones de validación de datos
+function validarDatoNum(dato) {
+  if (isNaN(dato) || dato <= 0) alert("Debe ser un número mayor que 0.");
+}
 
-        //condicional valor entradas segun edad
-        if (edadEntrada < 6) {
-          valorEntrada = descInfantes;
-        } else if (edadEntrada < 13) {
-          valorEntrada = entradaGral - entradaGral * descMenores;
-        } else if (edadEntrada >= 65) {
-          valorEntrada = entradaGral - entradaGral * descMayores;
-        }
-
-        //Impresion por pantalla de Datos ingresados
-        alert(
-          "Entrada " +
-            i +
-            ":\nDNI: " +
-            dniEntrada +
-            "\nNombre Visitante: " +
-            nombreEntrada +
-            " " +
-            apellidoEntrada +
-            "\nEdad del Visitante: " +
-            edadEntrada +
-            "\nValor Entrada: $" +
-            valorEntrada
-        );
-      }
-
-      //mensaje reserva exitosa
-      alert(
-        "Reserva Exitosa.!" +
-          nombre +
-          "!\nRecuerde asistir dentro de los 10 días posteriores de la reserva, con DNI de los visitantes sin excepción\nTu Número de reserva es " +
-          dni +
-          " \n¡Muchas gracias! Te esperamos! Parque Tecno ☼"
-      );
-
-      //Funciones de validación de datos
-      function validarDatoNum(dato) {
-        if (isNaN(dato) || dato <= 0) alert("Debe ser un número mayor que 0.");
-      }
-
-      function validarDatoStr(dato) {
-        if (!/^[a-zA-Z]+$/.test(dato) || dato.trim() === "") {
-          alert("Ingrese solo texto válido");
-        }
-      }
-
-      break;
-    case 2: // Tienda
-      let opcion;
-
-      do {
-        opcion = Number(
-          prompt(
-            "Selecciona una opción:\n1. Buscar por Categoria\n2. Buscar por Subcategoria\n3. Ver Informacion completa de un producto \n4. Agregar al Carrito \n5. Finalizar compra \n0. Volver al Menú Inicial"
-          )
-        );
-
-        //Switch Tienda
-        switch (opcion) {
-          case 1: //Buscar por Categoría
-            let opcionCategoria = Number(
-              prompt(
-                "Selecciona una Categoría:\n1. Indumentaria\n2. Accesorios \n0. Salir"
-              )
-            );
-            if (opcionCategoria === 1) {
-              let porCategoria = productos.filter((producto) =>
-                producto.categoria.includes("Indumentaria")
-              );
-              alert(listaProductos(porCategoria));
-            } else if (opcionCategoria === 2) {
-              let porCategoria = productos.filter((producto) =>
-                producto.categoria.includes("Accesorios")
-              );
-              alert(listaProductos(porCategoria));
-            } else {
-              alert("La opción ingresada en incorrecta");
-            }
-
-            break;
-
-          case 2: //Buscar por Subcategoria
-            let opcionSubcategoria = Number(
-              prompt(
-                "Selecciona una Subcategoría:\n1. Remeras\n2. Buzos \n3. Camperitas \n4. Lentes \n5. Collares\n0. Salir"
-              )
-            );
-            if (opcionSubcategoria === 1) {
-              let porSubcategoria = productos.filter((producto) =>
-                producto.subcategoria.includes("Remera")
-              );
-              alert(listaProductos(porSubcategoria));
-            } else if (opcionSubcategoria === 2) {
-              let porSubcategoria = productos.filter((producto) =>
-                producto.subcategoria.includes("Buzos")
-              );
-              alert(listaProductos(porSubcategoria));
-            } else if (opcionSubcategoria === 3) {
-              let porSubcategoria = productos.filter((producto) =>
-                producto.subcategoria.includes("Camperitas")
-              );
-              alert(listaProductos(porSubcategoria));
-            } else if (opcionSubcategoria === 4) {
-              let porSubcategoria = productos.filter((producto) =>
-                producto.subcategoria.includes("Lentes")
-              );
-              alert(listaProductos(porSubcategoria));
-            } else if (opcionSubcategoria === 5) {
-              let porSubcategoria = productos.filter((producto) =>
-                producto.subcategoria.includes("Collares")
-              );
-              alert(listaProductos(porSubcategoria));
-            } else {
-              alert("La opción ingresada en incorrecta");
-            }
-
-            break;
-
-          case 3: //Ver Informacion Completa del Producto
-            let productoBuscadoID = prompt("Ingrese ID del Producto:");
-            productoBuscado = buscarPorID(productoBuscadoID);
-
-            if (productoBuscado) {
-              mostrarInfoProducto(productoBuscado);
-            } else {
-              alert("Producto no encontrado. Por favor, ingrese un ID válido.");
-            }
-            break;
-
-          case 4: //Agregar al carrito
-            agregarProductoAlCarrito(productos, carrito, e);
-
-            break;
-
-          case 5: //Finalizar compra
-            finalizarCompra(carrito);
-            break;
-
-          case 0: // Volver al Menu
-            break;
-
-          default: // Opción no válida
-            alert("Opción no válida. Por favor, selecciona una opción válida.");
-            break;
-        }
-      } while (opcion !== 0);
-
-      break;
-    case 3: //Festeja tu cumple
-      alert(
-        "Has seleccionado Festeja tu cumple.\nProximamente más información"
-      );
-      break;
-    case 4: // Retira tu foto
-      alert("Has seleccionado Retirar tu foto.\nProximamente más información");
-      break;
-    case 0: // Salir
-      alert("Gracias por usar nuestro sistema. ¡Hasta luego!");
-      break;
-    default:
-      // Opción no válida
-      alert("Opción no válida. Por favor, selecciona una opción válida.");
-      break;
+function validarDatoStr(dato) {
+  if (!/^[a-zA-Z]+$/.test(dato) || dato.trim() === "") {
+    alert("Ingrese solo texto válido");
   }
-} while (opcion !== 0);
+} */
 
-//Funciones
 function listaProductos(productos) {
   return productos
     .map(
@@ -743,14 +622,15 @@ function buscarPorID(id) {
   return productos.find((producto) => producto.id === id);
 }
 
+// Crea Tarjetas ya Filtradas
 function filtrarYRenderizar(productos, carrito) {
-  // Crea Tarjetas ya Filtradas
   let productosFiltrados = productos.filter((producto) =>
     producto.nombre.includes(buscador.value)
   );
   renderizarProductos(productosFiltrados, carrito);
   return productosFiltrados;
 }
+
 
 function agregarProductoAlCarrito(productos, carrito, e) {
   let productoBuscado = productos.find(
@@ -775,8 +655,7 @@ function agregarProductoAlCarrito(productos, carrito, e) {
       productoEnCarrito.subtotal =
         productoEnCarrito.unidades * productoEnCarrito.precioUnitario;
       productoBuscado.stock--;
-      /*       alert("Se sumó una unidad más del producto al carrito");
-       */
+      
     } else {
       // Si no está en el carrito, agregarlo
       let productoNuevo = {
@@ -789,12 +668,13 @@ function agregarProductoAlCarrito(productos, carrito, e) {
       };
       carrito.push(productoNuevo);
       productoBuscado.stock--;
-      /*       alert("El producto se agregó al carrito.");
-       */
+      localStorage.setItem("carrito", JSON.stringify(carrito));
     }
     renderizarCarrito(carrito);
   }
 }
+
+
 
 function renderizarCarrito(productosEnCarrito) {
   let divCarrito = document.getElementById("carrito");
@@ -812,16 +692,18 @@ function renderizarCarrito(productosEnCarrito) {
   });
 }
 
+
+
 function finalizarCompra(carrito) {
   if (carrito.length === 0) {
-    alert("No hay productos en el carrito.");
+    /* alert("No hay productos en el carrito."); */
   } else {
     let totalUnidades = carrito.reduce((total, producto) => {
       return total + producto.unidades;
     }, 0);
 
     while (true) {
-      alert(
+      /* alert(
         "\nProductos en carrito: " +
           totalUnidades +
           "\n" +
@@ -829,17 +711,17 @@ function finalizarCompra(carrito) {
           "\nTotal: $" +
           carrito.reduce((total, producto) => {
             return total + producto.subtotal;
-          }, 0)
-      );
+          }, 0) 
+      );*/
 
       opcion = Number(
-        prompt(
+        /* prompt(
           "¿Estás seguro que querés finalizar la compra?\n1. Para continuar agregando productos\n0. Para Finalizar"
-        )
+        ) */
       );
 
       if (opcion === 0) {
-        alert("Compra realizada con Éxito!");
+        /* alert("Compra realizada con Éxito!"); */
         break;
       } else {
         break;
@@ -848,11 +730,11 @@ function finalizarCompra(carrito) {
   }
 }
 
-renderizarProductos(productos, carrito);
+
+
 
 //Crea tarjetas de cada producto
 function renderizarProductos(productos, carrito) {
-
   let contenedor = document.getElementById("contenedorProductos");
   contenedor.innerHTML = "";
 
@@ -868,18 +750,20 @@ function renderizarProductos(productos, carrito) {
     contenedor.appendChild(tarjeta);
 
     let botonAgregarAlCarrito = document.getElementById(producto.id);
-    botonAgregarAlCarrito.addEventListener("click", (e) =>
+    botonAgregarAlCarrito.addEventListener("click", (e) => {
       agregarProductoAlCarrito(productos, carrito, e)
-    );
+  });
   });
 }
 
 let buscador = document.getElementById("buscador"); // Casilla Buscador
-
 let botonBuscar = document.getElementById("botonBuscar"); //Boton Busqueda
-botonBuscar.addEventListener("click", (e) => filtrarYRenderizar(productos, carrito)); //Accion boton buscar
+botonBuscar.addEventListener("click", (e) =>
+  filtrarYRenderizar(productos, carrito)
+);
 
 let botonVerOcultar = document.getElementById("verOcultar");
+//Ver ocultar carrito
 botonVerOcultar.addEventListener("click", verOcultarCarrito);
 
 function verOcultarCarrito() {
@@ -892,21 +776,46 @@ function verOcultarCarrito() {
 
 let contenedorReservas = document.getElementById("contenedorReservas");
 let dniComprador = document.getElementById("dniComprador");
-
 let nombreComprador = document.getElementById("nombreComprador");
 
+//NombreComprador-Enter
 nombreComprador.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     saludoComprador();
   }
-});
+})
 
 function saludoComprador() {
   let saludo = document.createElement("div");
   saludo.innerHTML = `
-    <h3>Hola ${nombreComprador.value}!</h3>
-    <p>Estamos listos para empezar a reservar. Y vos? Comencemos:</p>
+  <h3>Hola ${nombreComprador.value}!</h3>
+  <p>Estamos listos para empezar a reservar. Y vos? Comencemos:</p>
     `;
 
   contenedorReservas.appendChild(saludo);
 }
+
+function mensajeError() {
+  let mensajeError = document.createElement("div");
+  mensajeError.innerHTML = `
+  <h3>Dato Incorrecto</h3>
+  <p>Por favor, ingrese un dato válido</p>
+    `;
+    
+  contenedorReservas.appendChild(mensajeError);
+}
+
+
+//Finalizar compra
+finalizarCompra(carrito);
+
+
+
+
+
+
+
+
+
+
+
